91e398511fafe2942daaaf468ec79d420046c31e
Scclegionx <hoanganhkemosabe@gmail.com>
2025-07-07 15:14:13 +0700

    tesst tesse

--- base/views.py
+++ base/views.py
@@ -26,7 +26,9 @@ def process_text(request):
         # Xử lý văn bản và trả về kết quả
         result = nlp_model.predict(text)
 
-        
+        print("test hihi")
+        print(result)
+
         return JsonResponse({
             'command': result['command'],
             'entities': result['entities'],
